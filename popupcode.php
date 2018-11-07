977bc7702cc3f8231908bd31267ccdb34d714f3e
vipindubey098 <44840435+vipindubey098@users.noreply.github.com>
2018-11-07 19:31:41 +0530

    Add files via upload

---
+++ popupcode.php
@@ -0,0 +1,50 @@
+<!--popup code starts-->
+<script type="text/javascript" language="javascript">
+function ccpop()
+{
+document.getElementById("overlay").style.display = 'block';
+}
+window.onload = ccpop;
+</script>
+<style>
+#overlay {
+background:url('https://4.bp.blogspot.com/-6nW7fK26v0Q/VO4CHaK134I/AAAAAAAAKCM/vVOfX9BsHoA/s1600/overlay.png');
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 99999;
+
+}
+.fb-box {
+box-shadow: 0px 0px 6px #ccc;
+-webkit-box-shadow: 0px 0px 6px #ccc;
+-o-box-shadow: 0px 0px 6px #ccc;
+background: #fff;
+}
+#ccpop {
+position:fixed;
+top: 100px;
+width:300px;
+margin-left:-150px;
+left:50%;
+height:400px;
+padding: 15px;
+z-index: 9999;
+color: #333;
+}
+</style>
+<div id="overlay" style="display:none;">
+<div id='ccpop'>
+	<input type="button" style="position:relative;" onclick="document.getElementById('overlay').style.display='none'" value="Close [x]" />
+<div class='fb-like-pop'>
+<div class="fb-box">
+<iframe allowtransparency='true' frameborder='0' scrolling='no' src='//www.facebook.com/plugins/likebox.php?href=https://www.facebook.com/educlashco/&width=270&height=250&colorscheme=light&show_faces=true&show_border=false&stream=false&header=true' style='border: none; overflow: hidden; width: 402px; height: auto; 	}'></iframe><center>
+	<h4><a href="https://www.facebook.com/groups/educlashco/" target="_blank">Join our Facebook Group</a></h4>
+	<h5>Even, <b><a href="https://educlash.com/join-educlash-com-whatsapp-group/" target="_blank">Click Here</a></b> To Join Whatsapp Group</h5>Follow:&nbsp;<i><b><a href="https://twitter.com/educlashco" target="_blank">Twitter</a></b> | <b><a href="https://www.instagram.com/educlashco/" target="_blank">Instagram</a></b> | <b><a href="https://t.me/educlashco" target="_blank">Telegram</a></b></i></center>
+	</div>
+</div><!--FB Like Box-->
+</div><!--ccpop-->
+</div><!--Overlay-->
+<!--End Of All-->
